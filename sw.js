584b12c4f04621ff1441bd6a24a39f49cb640f18
SkipShot <2360159951@qq.com>
2025-05-07 21:30:08 +0800

    Site updated: 2025-05-07 21:30:05

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1746619698735";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1746624570820";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
